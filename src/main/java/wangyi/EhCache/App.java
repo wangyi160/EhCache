f8626e329030af39c9ded37324fea40e000b7917
wangyi160 <wangyi160@hotmail.com>
2016-05-03 13:47:15 +0800

    first try

---
+++ src/main/java/wangyi/EhCache/App.java
@@ -0,0 +1,40 @@
+package wangyi.EhCache;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		CacheManager cacheManager = CacheManagerBuilder
+				.newCacheManagerBuilder()
+				.withCache(
+						"preConfigured",
+						CacheConfigurationBuilder.newCacheConfigurationBuilder(
+								Long.class, String.class,
+								ResourcePoolsBuilder.heap(100)).build())
+				.build(true);
+
+		Cache<Long, String> preConfigured = cacheManager.getCache(
+				"preConfigured", Long.class, String.class);
+
+		Cache<Long, String> myCache = cacheManager.createCache(
+				"myCache",
+				CacheConfigurationBuilder.newCacheConfigurationBuilder(
+						Long.class, String.class,
+						ResourcePoolsBuilder.heap(100)).build());
+
+		myCache.put(1L, "da one!");
+		String value = myCache.get(1L);
+
+		System.out.println(value);
+		
+		cacheManager.close();
+	}
+}
